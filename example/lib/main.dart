0f831b464146cc1c609ae16fda62d95c0372acb1
Saad Ardati <saadodi44@gmail.com>
2023-10-02 12:43:37 +0300

    Create storyboard example.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,7 @@
-import 'dart:math';
-
+import 'package:example/stories/scroll_phase_transition.dart';
+import 'package:example/stories/scroll_wheel_transition.dart';
 import 'package:flutter/material.dart';
-import 'package:hyper_effects/hyper_effects.dart';
+import 'package:storybook_flutter/storybook_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,78 +12,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'MyApp Demo',
+    return MaterialApp(
+      title: 'Hyper Effects Story Book',
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        // backgroundColor: Color(0xFFF0F1FA),
-        body: Center(
-          child: MyScreen(),
-        ),
+      home: Storybook(
+        stories: [
+          Story(
+            name: 'Scroll Phase Offset',
+            description: 'Offsetting elements based on the phase of the scroll',
+            builder: (context) => const ScrollPhaseTransition(),
+          ),
+          Story(
+            name: 'Scroll Wheel Transition',
+            description: 'Warping elements to mimic a cylindrical effect.',
+            builder: (context) => const ScrollWheelTransition(),
+          ),
+        ],
       ),
-    );
-  }
-}
-
-class MyScreen extends StatefulWidget {
-  const MyScreen({super.key});
-
-  @override
-  State<MyScreen> createState() => _MyScreenState();
-}
-
-class _MyScreenState extends State<MyScreen> {
-  Color randomColor(int index) {
-    final r = Random(index * 100).nextInt(255);
-    final g = Random(index * 200).nextInt(255);
-    final b = Random(index * 300).nextInt(255);
-    return Color.fromARGB(255, r, g, b);
-  }
-
-  bool toggle = true;
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        return GestureDetector(
-          onTap: () => setState(() => toggle = !toggle),
-          child: Container(
-            width: 350,
-            height: 350,
-            // height: Random(index * 100).nextDouble().clamp(0.1, 0.5) * 1000,
-            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              color: randomColor(index),
-              borderRadius: BorderRadius.circular(16),
-            ),
-            child: Text(
-              'Item $index',
-              style: const TextStyle(
-                fontSize: 24,
-                color: Colors.white,
-              ),
-            ),
-          )
-              .scale(toggle ? 1 : 0.8)
-              .rotate(toggle ? 0 : 90 * pi / 180)
-              .animate(
-                toggle: toggle,
-              )
-              .scrollTransition(
-                index,
-                (context, widget, event) =>
-                    widget.scale(event.phase.isIdentity ? 1 : 0.7).translateX(
-                          switch (event.phase) {
-                            ScrollPhase.identity => 0,
-                            ScrollPhase.topLeading => 200,
-                            ScrollPhase.bottomTrailing => -200,
-                          },
-                        ),
-              ),
-        );
-      },
     );
   }
 }
---
+++ example/lib/stories/scroll_wheel_transition.dart
@@ -0,0 +1,53 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:hyper_effects/hyper_effects.dart';
+
+class ScrollWheelTransition extends StatefulWidget {
+  const ScrollWheelTransition({super.key});
+
+  @override
+  State<ScrollWheelTransition> createState() => _ScrollWheelTransitionState();
+}
+
+class _ScrollWheelTransitionState extends State<ScrollWheelTransition> {
+  Color randomColor(int index) {
+    final r = Random(index * 100).nextInt(255);
+    final g = Random(index * 200).nextInt(255);
+    final b = Random(index * 300).nextInt(255);
+    return Color.fromARGB(255, r, g, b);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return Container(
+          width: 350,
+          height: 100,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: randomColor(index),
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Text(
+            'Item $index',
+            style: const TextStyle(
+              fontSize: 24,
+              color: Colors.white,
+            ),
+          ),
+        ).scrollTransition(
+            index,
+            (context, widget, event) => TransformEffect(
+                  rotateX: -90 * event.screenOffsetFraction * pi / 180,
+                  translateY: (event.screenOffsetFraction * -1) * 200,
+                  translateZ: event.screenOffsetFraction.abs() * 100,
+                  scaleX: 1 - (event.screenOffsetFraction.abs() / 2),
+                  depth: 0.002,
+                ).apply(context, widget));
+      },
+    );
+  }
+}
